b4e7fbb1b59b8ede76ee4d3fe4977e5e1e5113bb
mrxrmx <mrxrmx@super.lt>
2014-05-12 18:54:20 +0300

    Update mpi.sh

--- mpi.sh
+++ mpi.sh
@@ -2,7 +2,7 @@
 #SBATCH -p short
 #SBATCH -n16
 
-R_LIBS=/scratch/lustre/zemlys/lib64/R/library
+R_LIBS=/scratch/lustre/mare9467/lib64/R/library
 export R_LIBS
 
 mpirun --vanilla --slave -f /scratch/lustre/home/mare9467/mif_home/RRR/one.R
